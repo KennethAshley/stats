8b2eca319c86c4ca8bdf890c463891cdab62a997
Kenneth Ashley <ken.u.diggitt@gmail.com>
2019-08-13 00:50:41 -0400

    Add route

--- express/server.js
+++ express/server.js
@@ -6,8 +6,13 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(bodyParser.json());
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
