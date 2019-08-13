99007348e8e6c86a9dfa1d3c0760e2c8c2e974d5
Kenneth Ashley <ken.u.diggitt@gmail.com>
2019-08-13 00:47:39 -0400

    Update

--- express/server.js
+++ express/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-app.use('/', express.static('public'));
+app.use('/', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
